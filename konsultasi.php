6991830000b1c1fc7ac1e655ec5ccf8915c0324e
deniardiyanto <deniardi4@gmail.com>
2020-06-11 22:56:46 +0700

    merge konsultasi

--- konsultasi.php
+++ konsultasi.php
@@ -1,35 +1,96 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Konsultasi</title>
-	<link rel="stylesheet" type="text/css" href="model.css">
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-</head>
-<body>
-	<div class="header">
-		<div class="menu">
-			<table align="center">
-				<tr align="center">
-					<td width="350px"><a href="index.html" class="menu">B E R A N D A</a></td>
-					<td width="350px"><a class="menu">K O N S U L T A S I</a></td>
-					<td width="350px"><a href="admin/login.php" class="menu">L O G I N</a></td>
-				</tr>
-			</table>
-		</div>	
-	</div>
-	<div>
-		<div class="konten">
-			<p class="hal">Beranda > Konsultasi</p>
-			<hr>
-			<p class="judulkonten"><b>KONSULTASI</b></p>
-			<div class="garispendek gold"></div>
-		</div>
-		<br>
-	</div>
-</body>
-
-<div class="footer">
-	<p class="textf" >S I S T E M  P A K A R</p>
-  	<p align="center">DIAGNOSA PENYAKIT PADA ANJING</p>  
+<?php
+    $success = false;
+    $row = $db->get_row("SELECT * FROM tb_konsultasi ORDER BY ID DESC LIMIT 1");
+    
+    if($row) {
+        $jawaban = $row->jawaban;
+        $row = $db->get_row("SELECT * FROM tb_pengetahuan WHERE jika='$row->kode_gejala'");  
+        
+        if($jawaban=='Ya'){
+            if($row->ya_tanya){
+                $kode_gejala = $row->ya_tanya;
+            } else {
+                $kode_diagnosa = $row->ya_diagnosa;
+                $success = true;
+            }
+        } else {
+            if($row->tidak_tanya){
+                $kode_gejala = $row->tidak_tanya;
+            } else {
+                $kode_diagnosa = $row->tidak_diagnosa;
+                $success = true;
+            }
+        } 
+        
+        if($success) {
+            $row = $db->get_row("SELECT * FROM tb_diagnosa WHERE kode_diagnosa='$kode_diagnosa'");
+        } else {
+            $row = $db->get_row("SELECT * FROM tb_gejala WHERE kode_gejala='$kode_gejala'");
+        }   
+             
+    } else {
+        $start = true;
+        $row = $db->get_row("SELECT * FROM tb_pengetahuan p INNER JOIN tb_gejala g ON g.kode_gejala=p.jika ORDER BY ID LIMIT 1");        
+    }
+    
+    
+?>
+<div class="page-header">
+    <h1>Konsultasi</h1>
 </div>
-</html>
+
+<?php if($success) :?>
+<div class="panel panel-primary">
+    <div class="panel-heading"><h3 class="panel-title">Hasil Konsultasi</h3></div>
+    <div class="panel-body text-center">
+        <p>Adapun penyakit yang diderita kucing adalah</p>
+        <h3><?=$row->nama_diagnosa?></h3>
+        <form action="simpan.php" method="post">
+            <input type="hidden" name="kode_gejala" value="<?=$row->kode_gejala?>" />
+            <p>&nbsp;</p>
+            <p>
+                <button name="new" class="btn btn-primary" value="1"><span class="glyphicon glyphicon-refresh"></span> Konsultasi Lagi</button>
+            </p>
+            <p>
+                
+                <a class="btn btn-danger" href="?m=history"><span class="glyphicon glyphicon history"></span> history</a>
+
+            </p>
+            <?php
+require_once'functions.php';
+if($_POST){
+    header("location:index.php?m=Konsultasi");
+}
+?>
+        </form>
+    </div>
+</div>    
+<h3>Riwayat Pertanyaan</h3>
+<div class="list-group">
+    <?php 
+    $rows = $db->get_results("SELECT * FROM tb_konsultasi k INNER JOIN tb_gejala g ON g.kode_gejala=k.kode_gejala");
+    foreach($rows as $row):?>
+    <div class="list-group-item"><?=$row->ID?>. Apakah <?=strtolower($row->nama_gejala)?>? <strong><?=$row->jawaban?></strong></div>
+    <?php endforeach?>
+</div>
+<?php else:?>
+<div class="panel panel-primary">
+    <div class="panel-heading"><h3 class="panel-title">Jawablah pertanyaan berikut ini</h3></div>
+    <div class="panel-body">
+        <h3>Apakah <?=strtolower($row->nama_gejala)?>?</h3>
+        <form action="simpan.php" method="post">
+            <input type="hidden" name="kode_gejala" value="<?=$row->kode_gejala?>" />
+            <p>&nbsp;</p>
+            <p>
+                <button name="yes" class="btn btn-primary" value="1">Ya</button>
+                <button name="no" class="btn btn-danger" value="1">Tidak</button> 
+                <?php if(!$start):?>           
+                <button name="new" class="btn btn-info pull-right" value="1">Batal</button>
+                <?php endif?>
+            </p>
+        </form>
+    </div>
+
+</div>
+<?php endif?>
+    
---
+++ konsultasif.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Konsultasi</title>
+	<link rel="stylesheet" type="text/css" href="model.css">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+</head>
+<body>
+	<div class="header">
+		<div class="menu">
+			<table align="center">
+				<tr align="center">
+					<td width="350px"><a href="index.html" class="menu">B E R A N D A</a></td>
+					<td width="350px"><a class="menu">K O N S U L T A S I</a></td>
+					<td width="350px"><a href="admin/login.php" class="menu">L O G I N</a></td>
+				</tr>
+			</table>
+		</div>	
+	</div>
+	<div>
+		<div class="konten">
+			<p class="hal">Beranda > Konsultasi</p>
+			<hr>
+			<p class="judulkonten"><b>KONSULTASI</b></p>
+			<div class="garispendek gold"></div>
+		</div>
+		<br>
+	</div>
+</body>
+
+<div class="footer">
+	<p class="textf" >S I S T E M  P A K A R</p>
+  	<p align="center">DIAGNOSA PENYAKIT PADA ANJING</p>  
+</div>
+</html>
