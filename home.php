88a026e3499a35be142899478a3084603c613375
deniardiyanto <deniardi4@gmail.com>
2020-06-14 21:20:45 +0700

    add css and other

--- home.php
+++ home.php
@@ -1,42 +1,22 @@
-<div class="page-header">
-	<style>
-		.judul {
-			background-color: darkgray;
-		}
-		.navigasi {
-			background-color: darkgray;
-		}
-	</style>
-	<div class="judul">
-    <strong><h1 class="entry-title">Kucing Persia</h1></strong> 
-</div>
-</hr>
-</div>
-<div class="entry-content">
-	<right><img src="images/5.jpg" width="550" height="300" style="float:right; " /></right> </div> </br>
-
-<p><p style="font-family:'Verdana', Courier, monospace; color:#0000; text-align:justify">
-Kucing ini merupakan kucing domestik dengan karakteristik bulu panjang, wajah bulat dengan moncong yang pendek. Selain itu, ia juga memiliki sifat pemalas dan manja. Sesuai dengan namanya, ia berasal dari Persia yang mana merupakan nama lama dari Iran. Kucing hanya mampu menjilati bagian yang sakit dari luar saja, jika penyakit yang menyerang berada di dalam tentu kucing tidak dapat mengobati sendiri. Berikut adalah beberapa penyakit yang menyerang kucing.</p></p>
-<p><p style="font-family:'Verdana', Courier, monospace; color:#0000; text-align:justify"></br>Kucing Persia adalah salah satu ras kucing yang disukai oleh para pecinta kucing. Sebagai hewan karnivora kucing juga memiliki insting dan naluri berburu yang cukup tinggi, dengan memburu hewan pengerat yang lebih kecil dari ukuran kucing seperti tikus.</p>
-
-</br>
-
-
-</p>
-<div class="navigasi">
-<h2><right>Diagnosa Penyakit</h2></right> </div>
-<right><p style="font-family:Verdana, Geneva, sans-serif; text-align:justify;">
-	<p style="font-family:'Verdana', Courier, monospace; color:#00000; text-align:justify;">
-Pengertian umum diagnosa adalah penentuan jenis penyakit dengan meneliti (memeriksa) gejala-gajalanya. Dari segi medis, diagnosa adalah proses penentuan jenis penyakit berdasarkan tanda dan gejala dengan menggunakan cara dan alat seperti laboratorium, foto dan klinik. Penyakit adalah gangguan pada bagian tubuh, atau gangguan kesehatan. Dalam sistem pakar ini peneliti menggunakan penalaran berbasis aturan (Rule Based Reasoning). Pada penalaran berbasis aturan, representasi menggunakan IF-THEN, bentuk ini digunakan apabila kita memiliki sejumlah pengetahuan seorang pakar pada suatu permasalahan tertentu dan pakar dapat menyelesaikan masalah tersebut secara berurutan
-</br>
-	<img src="images/2.jpg" width="550" height="330" style="float:justify;" /></justify>
-	<img src="images/3.jpg" width="550" height="380" style="float:right;" />
-
-</br>
-
-	Metode yang paling sering dan mudah digunakan adalah memeriksa telinga dengan menggunakan alat yang disebut otoskop. Dengan alat ini dokter hewan dapat melihat keadaan telinga  bagian luar dan tengah termasuk saluran telinga.
-
-Tes lain bisa saja dilakukan dengan cara mengambil kotoran yang terdapat di dalam telinga, kemudian diperiksa menggunakan mikroskop. Dari kotoran tersebut di diketahui kondisi dan penyebab radang telinga.
-
-
-</div>
+<div class="tentang">
+		<p>
+				<p class="judulkonten"><b>SISTEM PAKAR</b></p>
+			Sistem pakar adalah sistem yang didesain dan diimplementasikan  dengan  bantuan  bahasa pemrograman  tertentu  untuk  dapat menyelesaikan masalah seperti yang dilakukan oleh para ahli atau pakar, sistem pakar sendiri merupakan paket perangkat lunak atau paket program computer yang ditujukan sebagai penyedia nasihat dan sarana bantu dalam memecahkan masalah di bidang-bidang spesialisasi tertentu seperti sains, perekayasaan, matematika, kedokteran, pendidikan dan sebagainya.  
+			Salah  satu  metode yang  yang  paling  umum untuk  merepresentasikan dan menyelesaikan masalah  untuk sistem pakar adalah dengan metode forward chaining. Strategi dengan pelacakan ke depan yang dimulai sekumpulan  fakta-fakta  pengetahuan,  memperoleh  fakta-fakta baru  menggunakan  aturan-aturan  dimana premis-premis  sesuai  dengan  fakta-fakta pengetahuan,  dan  meneruskan  prosesnya sampai  sebuah  tujuan  yang  ditetapkan  telah tercapai. Kadang disebut juga data driven karena interface engine mengunakan informasi yang ditentukan oleh user untuk memindah keseluruh jaringan dari logika ‘AND’ dan ‘OR’ sampai sebuah terminal ditentukan sebagai objek. 
+		</p>
+	</div>
+	<br><br><br>
+	<div class="konten2">
+		<table width="100%">
+			<tr>
+				<td width="80%" height="150px" align="center">
+					
+					<h1>S I S T E M  P A K A R</h1>
+					<p>DIAGNOSA PENYAKIT PADA ANJING</p>
+				</td>
+					
+				</td>
+				
+			</tr>
+		</table>
+	</div>
--- index.php
+++ index.php
@@ -23,28 +23,6 @@ include'functions.php';
 		
 	</div>
 	
-	<div class="tentang">
-		<p>
-				<p class="judulkonten"><b>SISTEM PAKAR</b></p>
-			Sistem pakar adalah sistem yang didesain dan diimplementasikan  dengan  bantuan  bahasa pemrograman  tertentu  untuk  dapat menyelesaikan masalah seperti yang dilakukan oleh para ahli atau pakar, sistem pakar sendiri merupakan paket perangkat lunak atau paket program computer yang ditujukan sebagai penyedia nasihat dan sarana bantu dalam memecahkan masalah di bidang-bidang spesialisasi tertentu seperti sains, perekayasaan, matematika, kedokteran, pendidikan dan sebagainya.  
-			Salah  satu  metode yang  yang  paling  umum untuk  merepresentasikan dan menyelesaikan masalah  untuk sistem pakar adalah dengan metode forward chaining. Strategi dengan pelacakan ke depan yang dimulai sekumpulan  fakta-fakta  pengetahuan,  memperoleh  fakta-fakta baru  menggunakan  aturan-aturan  dimana premis-premis  sesuai  dengan  fakta-fakta pengetahuan,  dan  meneruskan  prosesnya sampai  sebuah  tujuan  yang  ditetapkan  telah tercapai. Kadang disebut juga data driven karena interface engine mengunakan informasi yang ditentukan oleh user untuk memindah keseluruh jaringan dari logika ‘AND’ dan ‘OR’ sampai sebuah terminal ditentukan sebagai objek. 
-		</p>
-	</div>
-	<br><br><br>
-	<div class="konten2">
-		<table width="100%">
-			<tr>
-				<td width="80%" height="150px" align="center">
-					
-					<h1>S I S T E M  P A K A R</h1>
-					<p>DIAGNOSA PENYAKIT PADA ANJING</p>
-				</td>
-					
-				</td>
-				
-			</tr>
-		</table>
-	</div>
 	<div class="container">
     <?php
         // if(in_array($mod, array('diagnosa', 'gejala', 'pengetahuan', 'password')) && !$_SESSION[login])
@@ -52,7 +30,7 @@ include'functions.php';
         if(file_exists($mod.'.php'))
             include $mod.'.php';
         else
-            include 'index.php';
+            include 'home.php';
     ?>
     </div>
 	<div class="footer2" align="center">
