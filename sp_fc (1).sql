2f8c77ce75afefcc1bb9919a1506b1a0f259ad5b
deniardiyanto <deniardi4@gmail.com>
2020-08-18 10:54:12 +0700

    up

--- sp_fc (1).sql
+++ sp_fc (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 21, 2020 at 04:08 PM
+-- Generation Time: Jul 21, 2020 at 04:09 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.6
 
